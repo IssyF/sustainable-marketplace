7b155cf882e0b94b8893966b0873d969ee0a8c15
Isabel Foo <isabel.foo@hotmail.com>
2022-01-13 14:45:40 +0000

    associations added to models

--- app/models/seller.rb
+++ app/models/seller.rb
@@ -1,3 +1,6 @@
 class Seller < ApplicationRecord
   belongs_to :user
+  has_many :reviews
+  has_many :listings
+  has_one :sustainability_practice
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_one :buyer
+  has_one :seller
 end
