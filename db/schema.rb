086d872b35338f3f13ea7be66b4ce620345c51f2
Isabel Foo <isabel.foo@hotmail.com>
2022-01-13 16:29:21 +0000

    added boolean to listings to determine whether added to basket

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_12_174211) do
+ActiveRecord::Schema.define(version: 2022_01_13_162520) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 2022_01_12_174211) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.text "description", null: false
+    t.boolean "in_basket", default: false
     t.index ["seller_id"], name: "index_listings_on_seller_id"
   end
 
