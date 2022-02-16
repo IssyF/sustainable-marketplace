5b004c0642973943bab4041366b37c52b3462d2c
Isabel Foo <isabel.foo@hotmail.com>
2022-02-16 16:41:51 +0000

    listings cards html and css with responsive css grid - applied so far on new in page

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,4 +1,10 @@
 class ListingsController < ApplicationController
   def new_in
+    shuffled_listings = Listing.all.shuffle
+    @listings = shuffled_listings.first(15)
   end
+
+  def show
+  end
+
 end
